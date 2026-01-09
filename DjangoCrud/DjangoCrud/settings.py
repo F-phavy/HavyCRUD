9b89bbf1ce5d881e08b56fa270e550a8c0d8352e
Favour Samuel <your_email@example.com>
2026-01-09 15:50:00 +0100

    Fixed app path for employee module

--- DjangoCrud/DjangoCrud/settings.py
+++ DjangoCrud/DjangoCrud/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employee',
+    'DjangoCrud.employee',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
